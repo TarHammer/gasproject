03a0bdc80a42da0d7076f4f09f3dd5f871d4dc24
TarHammer <d.tarasconi@gmail.com>
2019-05-05 16:12:11 +0200

    Add files via upload

---
+++ SLAVEDEF2.2.ino
@@ -0,0 +1,155 @@
+#include <Wire.h>
+
+
+int check1;
+char buff5[7];
+char buff6[7];
+char buff7[7];
+char buff8[7];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Wire.begin(2); // any 7-bit Slave address
+  Serial.begin(9600);
+  Wire.onReceive(rispondi1); //Interrupt for handling incomming requests
+  Wire.onRequest(richiesta1);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  
+}
+
+
+void rispondi1()
+{
+  check1=Wire.read();
+}
+
+void richiesta1()
+{
+  if(check1==5)
+  {
+     
+    float sensor_voltMQ4; //Define variable for sensor voltage
+    float RS_gasMQ4; //Define variable for sensor resistance
+    float ratioMQ4; //Define variable for ratio
+    float sensorValueMQ4 = analogRead(A0); //Read analog values of sensor
+    sensor_voltMQ4 = sensorValueMQ4 * (5.0 / 1023.0); //Convert analog values to voltage
+    RS_gasMQ4 = ((5.0 * 10.0) / sensor_voltMQ4) - 10.0; //Get value of RS in a gas
+    ratioMQ4 = RS_gasMQ4 / 64.60;   // Get ratio RS_gas/RS_air
+
+    dtostrf(ratioMQ4,7,4, buff5);
+
+    Serial.print("Valore mq4: ");
+    Serial.println(buff5);
+    Wire.write(buff5);
+    
+    
+  }
+
+
+  if(check1==6)
+  {
+     
+    float sensor_voltMQ2;
+    float RS_gasMQ2; // Get value of RS in a GAS
+    float ratioMQ2; // Get ratio RS_GAS/RS_air
+    int sensorValueMQ2 = analogRead(A1);
+    sensor_voltMQ2=(float)sensorValueMQ2/1024*5.0;
+    RS_gasMQ2 = (5.0-sensor_voltMQ2)/sensor_voltMQ2; // omit * RL
+
+          
+    ratioMQ2 = RS_gasMQ2/1.01;  // ratio = RS/R0
+    dtostrf(ratioMQ2,7,4, buff6);
+    Serial.print("Valore mq2: ");
+    Serial.println(buff6);
+    Wire.write(buff6);
+    
+    
+  }
+
+
+
+
+  if(check1==7)
+  {
+     
+    float sensor_voltMQ6;
+    float RS_gasMQ6; // Get value of RS in a GAS
+    float ratioMQ6; // Get ratio RS_GAS/RS_air
+    float LPG_PPMMQ6;
+ 
+    int sensorValueMQ6 = analogRead(A2);
+ 
+    sensor_voltMQ6=(float)sensorValueMQ6/1024*5.0;
+ 
+    RS_gasMQ6 = (5.0-sensor_voltMQ6)/sensor_voltMQ6; // omit *RL /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
+ 
+    ratioMQ6 = RS_gasMQ6/1.55;  // ratio = RS/R0
+    dtostrf(ratioMQ6,7,4, buff7);
+ 
+    Serial.print("LPG PPM = ");
+    Serial.println(buff7);
+    Serial.print("\n\n");
+    Wire.write(buff7);
+    
+
+    
+    
+    
+  }
+
+  if(check1==8)
+  {
+     
+    float sensor_voltMQ135;
+    float CO2_PPM;
+    float RS_gasMQ135; // Get value of RS in a GAS
+    float ratioMQ135; // Get ratio RS_GAS/RS_air
+    
+ 
+    int sensorValueMQ135 = analogRead(A3);
+ 
+    sensor_voltMQ135=(float)sensorValueMQ135/1024*5.0;
+ 
+    RS_gasMQ135 = (5.0-sensor_voltMQ135)/sensor_voltMQ135; // omit *RL /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
+ 
+    ratioMQ135= RS_gasMQ135/76.63;  // ratio = RS/R0
+    
+    
+    CO2_PPM= (146.15*(2.868-ratioMQ135)+10);
+    dtostrf(CO2_PPM,7,4, buff8);
+ 
+    Serial.print("co2 PPM = ");
+    Serial.println(buff8);
+    Serial.print("\n\n");
+    Wire.write(buff8);
+    
+
+    
+    
+    
+  }
+
+
+
+
+
+  
+
+
+  
+
+
+
+
+
+
+  
+}
---
+++ SLAVE_DEF2.ino
@@ -0,0 +1,134 @@
+#include <MQ7.h>
+
+#include <Wire.h>
+
+//DICHIARO GLI ARRAY DI CHAR CHE CONTERRANNO IL FLOAT CONVERTITO  
+char buff[7];
+char buff2[7];
+char buff3[7];
+char buff4[7];
+
+
+
+float monossido;
+float mq9;
+float mq5;
+float mq3;
+float mq4;
+int check;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(1); // any 7-bit Slave address
+  Serial.begin(9600);
+  Wire.onReceive(rispondi); //quando il master inizia a scrivere, la funzione rispondi viene eseguita
+  Wire.onRequest(richiesta);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+void rispondi()
+{
+  check=Wire.read();//leggo la variabile inviata dal master e in base ad essa eseguo un determinato if
+ 
+}
+
+void richiesta()
+{
+  if(check==1)
+  {
+    MQ7 mq7(A0, 5.0);
+    float valueMQ7 = mq7.getPPM();
+    dtostrf(valueMQ7,7,4, buff);
+    Serial.println("----------------------------------------------------");
+    Serial.print("Valore Monossido: ");
+    Serial.println(buff);
+    Wire.write(buff);
+    delay(100);
+  }
+
+  if(check==2)
+  {
+    //calcoli vari ed eventuali per trovare il rapporto tra 
+    //il vaore di resistenza del sensore in aria "pulita"(r0) e 
+    //il valore varaibile di resistenza che il valore ha in presenza di gas(rs)
+    float sensor_volt;
+    float RS_gas; // Get value of RS in a GAS
+    float ratio; // Get ratio RS_GAS/RS_air
+    int sensorValue = analogRead(A1);
+    sensor_volt=(float)sensorValue/1024*5.0;
+    RS_gas = (5.0-sensor_volt)/sensor_volt; // omit *RL
+
+    ratio = RS_gas/1.18;  // ratio = RS/R0
+        
+    dtostrf(ratio,7,4, buff2); //trasformo il float "ratio" in un array di 7 caratteri e dimensione massima 4 byte
+    Serial.print("valore mq9: ");
+    Serial.println(buff2);
+    Wire.write(buff2);//invio al master la variabile convertita
+    delay(100);
+  }
+
+  if(check==3)//MQ5
+  {
+    float sensor_voltMQ5;
+    float RS_gasMQ5; // Get value of RS in a GAS
+    float ratioMQ5; // Get ratio RS_GAS/RS_air
+    int sensorValueMQ5 = analogRead(A2);
+    sensor_voltMQ5=(float)sensorValueMQ5/1024*5.0;
+    RS_gasMQ5 = (5.0-sensor_voltMQ5)/sensor_voltMQ5; // omit *RL
+
+          
+    ratioMQ5 = RS_gasMQ5/3.10;  // ratio = RS/3.10
+    dtostrf(ratioMQ5,7,4, buff3);
+         
+
+    Serial.print("Valore mq5: ");
+    Serial.println(buff3);
+    Wire.write(buff3);
+    
+  }
+
+  if(check==4)//MQ3
+  {
+    float sensor_voltMQ3;
+    float RS_gasMQ3; // Get value of RS in a GAS
+    float ratioMQ3; // Get ratio RS_GAS/RS_air
+    int sensorValueMQ3 = analogRead(A3);
+    sensor_voltMQ3=(float)sensorValueMQ3/1024*5.0;
+    RS_gasMQ3 = (5.0-sensor_voltMQ3)/sensor_voltMQ3; // omit *RL
+
+    /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
+    ratioMQ3 = RS_gasMQ3/0.22;  // ratio = RS/R0
+    dtostrf(ratioMQ3,7,4, buff4);
+         
+
+    Serial.print("Valore mq3: ");
+    Serial.println(buff4);
+    Wire.write(buff4);
+    
+  }
+
+
+
+
+  
+
+
+  
+
+  
+  
+}
+
+
+
+    
+  
+  
+
+  
+
+
